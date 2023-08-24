d7178a7494087ddb5bb16d10a3af58bbd5f4f75f
CHIMEZIE JOSHUA ONYERO <chimeziejoshuaonyero999@gmail.com>
2023-08-23 18:08:20 -0700

    Task 1 updated

--- simple_shell_01.c
+++ simple_shell_01.c
@@ -4,112 +4,105 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_INPUT_LENGTH 1024
-#define MAX_ARGS 64
+#define BUFFER_SIZE 1024
 
 /**
- * remove_newline - Removes the newline character from a string.
- * @str: The string to modify.
+ * cleanup - Frees allocated memory.
+ * @input_buffer: The buffer to be cleaned up.
  */
-void remove_newline(char *str)
+void cleanup(char *input_buffer)
 {
-	/* Find the newline character */
-	char *newline = strchr(str, '\n');
-
-	/* If found, replace it with null terminator */
-	if (newline)
-	{
-		*newline = '\0';
-	}
+	free(input_buffer);
 }
 
 /**
- * execute_command - Executes a given command.
- * @cmd: The command to execute.
- *
- * Return: 1 if executed successfully, 0 if not.
+ * execute_command - Executes the given command using execve.
+ * @command: The command to execute.
  */
-int execute_command(char *cmd)
+void execute_command(const char *command)
 {
-	/* Check if the command is executable */
-	if (access(cmd, X_OK) == 0)
-	{
-		pid_t child_pid = fork();
+	char **args = NULL;
+	int arg_count = 2;
 
-		/* Check for fork error */
+	args = (char **)malloc(arg_count * sizeof(char *));
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	args[0] = strdup(command);
+	if (args[0] == NULL)
+	{
+		perror("strdup");
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+
+	args[1] = NULL;
+
+	if (execve(command, args, NULL) == -1)
+	{
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+
+	free(args[0]);
+	free(args);
+}
+
+/**
+ * main_loop - Main loop for shell execution.
+ */
+void main_loop(void)
+{
+	char *input_buffer;
+	size_t bufsize = BUFFER_SIZE;
+	pid_t child_pid;
+
+	while (1)
+	{
+		input_buffer = (char *)malloc(bufsize * sizeof(char));
+		if (input_buffer == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("myshell$ ");
+		getline(&input_buffer, &bufsize, stdin);
+		input_buffer[strcspn(input_buffer, "\n")] = '\0';
+
+		if (strcmp(input_buffer, "exit") == 0)
+		{
+			cleanup(input_buffer);
+			exit(EXIT_SUCCESS);
+		}
+
+		child_pid = fork();
 		if (child_pid == -1)
 		{
 			perror("fork");
+			cleanup(input_buffer);
 			exit(EXIT_FAILURE);
 		}
-		/* Child process */
-		else if (child_pid == 0)
-		{
-			/* Execute the command */
-			if (execlp(cmd, cmd, (char *)NULL) == -1)
-			{
-				perror("execlp");
-				exit(EXIT_FAILURE);
-			}
-		}
-		/* Parent process */
+
+		if (child_pid == 0)
+			execute_command(input_buffer); /* Child process */
 		else
 		{
-			wait(NULL);
+			wait(NULL); /* Parent process */
+			cleanup(input_buffer);
 		}
-		return (1);
-	}
-	else
-	{
-		printf("%s: command not found\n", cmd);
-		return (0);
 	}
 }
 
 /**
- * main - Entry point of the simple shell program.
- *
+ * main - Entry point for the simple shell program.
  * Return: Always 0.
  */
 int main(void)
 {
-	char user_input[MAX_INPUT_LENGTH];
-
-	while (1)
-	{
-		printf("#myshell$ ");
-
-		/* Get user input */
-		if (!fgets(user_input, MAX_INPUT_LENGTH, stdin))
-		{
-			/* Check for end of file */
-			if (feof(stdin))
-			{
-				printf("\n");
-				break;
-			}
-			perror("fgets");
-			exit(EXIT_FAILURE);
-		}
-
-		/* Remove newline character */
-		remove_newline(user_input);
-
-		/* Check for exit command */
-		if (strcmp(user_input, "exit") == 0)
-		{
-			exit(EXIT_SUCCESS);
-		}
-
-		/* Execute the command */
-		if (strlen(user_input) > 0)
-		{
-			if (!execute_command(user_input))
-			{
-				continue;
-			}
-		}
-	}
-
-	return (EXIT_SUCCESS);
+	main_loop();
+	return (0);
 }
